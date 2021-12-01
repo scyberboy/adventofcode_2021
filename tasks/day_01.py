94ab9a95024741d163f586309f9a506966fecf24
Stoycho Petrov <scyberboy@users.noreply.github.com>
2021-12-02 01:39:14 +0200

    solution for (a) and (b): a - using map over input data b - using zip over input data, then map over the result

--- tasks/day_01.py
+++ tasks/day_01.py
@@ -1,3 +1,4 @@
+import operator
 import sys
 import os
 import re
@@ -6,7 +7,7 @@ input_data = []
 
 filename = os.path.basename(__file__)
 day_nr = re.search(r"\d+", filename).group()
-print("day_nr:", day_nr)
+print("day_nr", day_nr)
 
 
 def read_input():
@@ -14,10 +15,39 @@ def read_input():
         input_data.append(int(line.strip()))
 
 
+def find_solution_a():
+    reduced_data_it = filter(bool, map(operator.lt, input_data, input_data[1:]))
+    reduced_data = list(reduced_data_it)
+    reduced_data_len = len(reduced_data)
+
+    # print("reduced_data({}): {}".format(reduced_data_len, reduced_data))
+
+    return reduced_data_len
+
+
+def find_solution_b():
+    sums_list = list(map(sum, zip(input_data, input_data[1:], input_data[2:])))
+    # print("sums_list", sums_list)
+    reduced_data_it = filter(bool, map(operator.lt, sums_list, sums_list[1:]))
+    reduced_data = list(reduced_data_it)
+    reduced_data_len = len(reduced_data)
+
+    # print("reduced_data({}): {}".format(reduced_data_len, reduced_data))
+
+    return reduced_data_len
+
+
 def do_main():
 
     read_input()
-    print(input_data)
+    # print("input_data", input_data)
+
+    result_a = find_solution_a()
+    print("result_a", result_a)
+
+    result_b = find_solution_b()
+    print("result_a", result_b)
+
 
 if __name__ == "__main__":
     # execute only if run as a script
