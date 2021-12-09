874c3f9a6cd2990d0c422fbba2b26d8ff1fdfd3c
Stoycho Petrov <scyberboy@users.noreply.github.com>
2021-12-09 15:54:25 +0200

    day 7 - performance optimization

--- tasks/day_07.py
+++ tasks/day_07.py
@@ -1,4 +1,6 @@
+import time
 from collections import Counter
+import numpy as np
 import os
 import re
 import sys
@@ -23,21 +25,32 @@ def calc_cost_to_target(raci_to_move, target, consumption="linear"):
     :return: the cost to move all elements from raci_to_move to target
     """
 
+    # note: sbora na S:E e
+    # 			<sbora na granicite> po <polovinata ot broq>
+    # chetni: 	SUM = (S + E) * (E - S) / 2
+    # nechetni: 	<sbora na granicite> po <polovinata ot broq> + <srednoto chislo ot intervala)
+    # 			SUM = (S + E) * (E - S) / 2 + ((E - S) / 2 + 1)
+
     total_cost = 0
     for source, qty in raci_to_move.items():
         dist = abs(source - target)
-        if consumption == "linear":
+        if consumption == "linear":  # it's linear progression
             total_cost += (dist * qty)
-        else:  # it's algebraic progression
-            total_cost += (sum(range(1, dist+1)) * qty)
+        else:  # it's arithmetic progression
+            # Move from 16 to 5: 66 fuel
+            # Move from 1 to 5: 10 fuel
+            # Move from 2 to 5: 6 fuel
+            if dist % 2 == 0:  # chetni
+                adder = (1 + dist) * dist // 2
+            else:  # nechetni
+                adder = (1 + dist) * (dist // 2) + (dist // 2 + 1)
+            total_cost += (int(adder) * qty)
 
     return total_cost
 
 
 def find_solution_a(data, type_of_consumption="linear"):
     # type:(list, str) -> int
-
-    # get safe copy (don't alter the outside collection)
 
     initial_cntr = Counter(data)
     # print("initial counter: {}".format(initial_cntr))
@@ -58,7 +71,70 @@ def find_solution_a(data, type_of_consumption="linear"):
 
     result_cntr = Counter(cost_to_target)
     answer = result_cntr.most_common()
-    # print("all possible vals sorted, {} consumption: {}". format(type_of_consumption, answer))
+    print("last 10 MCV, {} consumption: {}". format(type_of_consumption, answer[-10:]))
+
+    # take the last one (the rarest == cheapest)
+    (_, val) = answer[-1]
+
+    return val
+
+
+def generate_candidates_list(starting_coord, min_coord, max_coord):
+    # type:(int, int, int) -> list
+    result = list()
+    result.append(int(starting_coord))
+    # we'll generate thus many pairs growing +1/-1 from starting coord
+    limit = min(abs(starting_coord-min_coord), abs(starting_coord-max_coord))
+    for adder in range(1, int(limit)):
+        result.append(int(starting_coord) + adder)
+        result.append(int(starting_coord) - adder)
+
+    return result
+
+
+def find_solution_b(data, type_of_consumption="linear"):
+    # type:(list, str) -> int
+
+    # Won't use brute force, but will choose the candidates 1 by 1
+    # Starting from the average or median of whole input values (the crab's coordinates)
+    # for linear consumption - median (indeed, it's almost sure this is the exact point we need)
+    # for arithmetic progressive consumption - average, then go both directions by +1/-1
+    # After 10 operations observe the result and if it's 'сходим', t.e. constantly increasing
+    # stop and take the first (smallest) calculated value.
+
+    most_common_dict = {k: v for (k, v) in Counter(data).most_common()}
+    min_coord = min(data)
+    max_coord = max(data)
+    starting_coord = np.median(data) if type_of_consumption == "linear" else np.average(data)
+
+    # start calculating the costs from the most common point consecutively
+    cost_to_target = dict()
+    current_winner = float("inf")
+    for coord in generate_candidates_list(starting_coord, min_coord, max_coord):
+        cost_to_target[coord] = calc_cost_to_target(most_common_dict, coord, type_of_consumption)
+        # print("cost_to_target: {}".format(cost_to_target))
+
+        # update the current winner if such have just been found
+        if cost_to_target[coord] < current_winner:
+            current_winner = cost_to_target[coord]
+
+        # check every X iterations, if the winner is the same. If it is - stop (use it)
+
+        # NB: this optimization is kind if irrelevant (non-significant)
+        #   so, rather skip it for the current set of data
+
+        # if len(cost_to_target) % 1000 == 0:
+        #     tmp_cntr = Counter(cost_to_target)
+        #     tmp_answer = tmp_cntr.most_common()
+        #     # print("\ttmp_answer[-10:]: {}".format(tmp_answer[-10:]))
+        #     (_, tmp_val) = tmp_answer[-1]
+        #
+        #     if tmp_val == current_winner:
+        #         break
+
+    result_cntr = Counter(cost_to_target)
+    answer = result_cntr.most_common()
+    print("last 10 MCV, {} consumption: {}".format(type_of_consumption, answer[-10:]))
 
     # take the last one (the rarest == cheapest)
     (_, val) = answer[-1]
@@ -72,11 +148,25 @@ def do_main():
 
     # use the smart solution for both parts :)
 
-    result_a = find_solution_a(raci)
-    print("result_a:", result_a)
+    print("--------")
+    result_a = find_solution_a(raci, type_of_consumption="linear")
+    print("result_a(brute):", result_a)
+    print("Total elapsed: {} sec.".format(time.process_time()))
 
-    result_b = find_solution_a(raci, type_of_consumption="algebraic")
-    print("result_b:", result_b)
+    print("--------")
+    result_b = find_solution_a(raci, type_of_consumption="arithmetic")
+    print("result_b(brute):", result_b)
+    print("Total elapsed: {} sec.".format(time.process_time()))
+
+    print("--------")
+    result_a = find_solution_b(raci, type_of_consumption="linear")
+    print("result_a(smart):", result_a)
+    print("Total elapsed: {} sec.".format(time.process_time()))
+
+    print("--------")
+    result_b = find_solution_b(raci, type_of_consumption="arithmetic")
+    print("result_b(smart):", result_b)
+    print("Total elapsed: {} sec.".format(time.process_time()))
 
 
 if __name__ == "__main__":
