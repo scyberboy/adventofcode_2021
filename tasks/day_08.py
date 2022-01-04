94af6d27e80458a29d44a315e1e8c8b7255d121b
Stoycho Petrov <scyberboy@users.noreply.github.com>
2022-01-04 10:18:45 +0200

    day 08 - solution (a)

--- tasks/day_08.py
+++ tasks/day_08.py
@@ -1,19 +1,42 @@
 import os
 import re
 import sys
+import time
 
 
-def read_input():
-    # Read lines input, raci -> 4,3,2,1,5,6,3....
-    # return list with integers
+def read_input() -> tuple[list[list[str]], list[list[str]]]:
+    """
 
-    data = [int(elem) for elem in sys.stdin.readline().split(",")]
+    """
+    # Read lines input:
+    # be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    # edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 
-    return data
+    # keep the dot's as list of two elements tuple (int, int) - (0, 5)
+    wire_signals = list()
+    # instructions as list of two elements (int, int) - (0, 7), where first is (0->x or 1->y), second is the axis
+    segment_signals = list()
+
+    while True:
+        line = sys.stdin.readline()
+        if line == "\n" or line == "":
+            break
+        group = line.strip().split(" | ")
+        wire_signals.append([code for code in group[0].split(" ")])
+        segment_signals.append([code for code in group[1].split(" ")])
+
+    # return two lists:
+    return wire_signals, segment_signals
 
 
-def find_solution_a():
-    pass
+def find_solution_a(segment_signals: list[list[str]]) -> int:
+    # count the 2, 3, 4 and 7 length strings which corresponds
+    # to number 1, 7, 4 and 8
+    cnt = 0
+    for code in [code for line in segment_signals for code in line]:
+        cnt += 1 if len(code) in [2, 3, 4, 7] else 0
+
+    return cnt
 
 
 def find_solution_b():
@@ -21,16 +44,32 @@ def find_solution_b():
 
 
 def do_main():
+    prev_time = time.process_time()
 
-    raci = read_input()
+    print("start reading input...")
+    wire_signals, segment_signals = read_input()
+    cur_time = time.process_time()
+    diff = cur_time - prev_time
+    prev_time = cur_time
+    print("[{}] took: {} sec.".format(cur_time, diff))
 
-    # use the smart solution for both parts :)
+    # print("wire_signals: {}".format(wire_signals))
+    # print("segment_signals: {}".format(segment_signals))
 
-    result_a = find_solution_a()
+    print("find_solution_a...")
+    result_a = find_solution_a(segment_signals)
     print("result_a:", result_a)
+    cur_time = time.process_time()
+    diff = cur_time - prev_time
+    prev_time = cur_time
+    print("[{}] took: {} sec.".format(cur_time, diff))
 
+    print("find_solution_b...")
     result_b = find_solution_b()
     print("result_b:", result_b)
+    cur_time = time.process_time()
+    diff = cur_time - prev_time
+    print("[{}] took: {} sec.".format(cur_time, diff))
 
 
 if __name__ == "__main__":
